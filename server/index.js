b96606c7999dc4a3c1792e481a43a57809159f40
Ayush Shrivastava <ayushshrivastava7903@gmail.com>
2025-08-04 00:25:05 +0530

    allowed all origins temporarily

--- server/index.js
+++ server/index.js
@@ -29,14 +29,15 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  // origin: function (origin, callback) {
+  //   if (!origin || allowedOrigins.includes(origin)) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  // credentials: true
+   origin: '*'
 }));
 
 
